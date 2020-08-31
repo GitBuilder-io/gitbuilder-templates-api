211d42d5c928ef804f6dd86b04d2d4ea691e8e61
Ben Sykes <ben@sykesdev.ca>
2020-08-31 11:10:07 -0400

    fixed image entrypoint script

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -3,8 +3,11 @@
 # By: Benjamin Sykes (SystemFiles)
 
 # Create required directories for templates
-mkdir -p /data
-mkdir -p /data/python
-mkdir -p /data/nodejs
+if [[ -z $TEMPLATES_DIR ]] ; then
+    TEMPLATES_DIR='/data/templates'
+fi
+
+mkdir -p ${TEMPLATES_DIR}/python
+mkdir -p ${TEMPLATES_DIR}/nodejs
 
 cpulimit -l 50 node ./server.js
